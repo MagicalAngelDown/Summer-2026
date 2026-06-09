4863d07567389f378b5961328a701ca7232a8b67
MagicalAngelDown <brooke1998lee@gmail.com>
2026-06-08 23:54:37 -0400

    Finished Lab one, starting lab 2

--- LillyLab1/index.php
+++ LillyLab1/index.php
@@ -5,11 +5,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <title>Brooke Lilly</title>
+        <link rel="icon" type="image/x-icon" href="/img/myriad_icon.png">
         
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link href="styles.css" rel="stylesheet"> 
+        <link href="style.css" rel="stylesheet"> 
     </head>
            
     <body>
@@ -18,7 +19,6 @@
                 <header class="w3-container" >
                     <div class="headerbg w3-card-4">
                         <h1>Brooke Lilly</h1>
-                        <h3>Middle GA State University</h3>
                     </div>
                 </header>
 
@@ -26,32 +26,103 @@
 
                 <nav class="w3-container">
                     <ul>
-                        <li><h3><a href="index.htm">Home</a> </h3></li>
-                        <li><h3><a href="resume.htm">Resume</a></h3></li>
-                        <li><h3><a href="interests.htm">Interests</a></h3></li>
+                        <li><h3><a href="index.php">Home</a> </h3></li>
+                        <li><h3><a href="portfolio.php">Portfolio</a></h3></li>
+                        <li><h3><a href="interests.php">Interests</a></h3></li>
                     </ul>
                 </nav>
 
                 <div class="clear"></div>
 
                 <main class="w3-container">
+                    <h1>Welcome to My Website</h1>
+                    <p>This is going to be my playground for experimenting with web development! Although I will be using this website to showcase my work, creativity and will link other websites I have already made.</p>
 
+
+                    <h3>Project Timeline</h3>
+                        <table class="table table-striped">
+                            <thead>
+                                <tr>
+                                    <th>Project</th>
+                                    <th>Status</th>
+                                    <th>Goal</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Rebuilding</td>
+                                    <td>In Progress</td>
+                                    <td>Rebuilding this skeleton to match this assignment and same with Styles.CSS</td>
+                                </tr>
+                                <tr>
+                                    <td>Portfolio</td>
+                                    <td>Planned</td>
+                                    <td>Adding more content, such as my personal website which is already published and our gaming website.</td>
+                                </tr>
+                                <tr>
+                                    <td>Interests Page</td>
+                                    <td>Planned</td>
+                                    <td>Adding an interests page that is more in depth than the one in my website.</td>
+                                </tr>
+                                 <tr>
+                                    <td>Combining</td>
+                                    <td>Planned</td>
+                                    <td>Combining this and my webpage that is already published.</td>
+                                </tr>
+                            </tbody>
+                        </table>
+
+                    <h3>Skills</h3>
+                    <p>Here are some of the skills I have developed:</p>
+                    <ul>
+                        <li>HTML</li>
+                        <li>CSS</li>
+                        <li>JavaScript</li>
+                        <li>Java</li>
+                        <li>Python</li>
+                    </ul>
+
+                    <section class="row">
+                        <article class="column">
+                            <h2>Why Web Development?</h2>
+                            <p>I first was going into nursing when I graduated from High School. Although my love of gaming and technology led me to explore web development as a career path. 
+                                Also the fact I found out I am squimish when it comes to nursing.</p>
+                            <img src="img/EasterPhoto.jpg" alt="Family Picture Easter">    
+                        </article>
+
+                        <aside class="column">
+                            <h2>Fun Facts about Me!</h2>
+                            <ul>
+                                <li>I have a 2 dogs and a cat. Our cat is named Lola, and our two dogs are Artemis and Zephyr.</li>
+                                <li>I currently have a 10 month old baby boy, named Michael</li>
+                                <li>My favorite game is Minecraft, Dead by Daylight and Sims 4.</li>
+                                <li>Me and My husband own our own Discord Server with 2000 members. With about 3 active playing servers: Garrys mod, Minecraft, and Project Zomboid</li>
+                            </ul>
+
+                            <img src="img/Cuddles.jpg" alt="Family Picture Cuddles">
+                        </aside>
+                    </section>
+                        
                 </main>
 
                 <div class="clear"></div>
 
                 <footer class="w3-container">
-
+                    <figure>
+                        <strong>Published Website:</strong> <a href="https://brookelilly.com/">brookelilly.com</a>
+                        <img src="img/Myriad.png" alt="myriad" class="left" width="100"/>
+                            <figcaption>
+                                <strong>
+                                    Co-Owner of Myriad Gaming
+                                    Established Since 2018
+                                </strong>
+                            </figcaption>
+                    </figure>
                 </footer>
 
                 <div class="clear"></div>
 
             </div>  
-        
-        <script src="scripts.js"></script>
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>      
     
     </body>        
 </html>
